082b663b2ca66159a5021c789b014c9aa192256f
Youn-Hee Han <yhhan@Youn-Hee-Hanui-MacBookAir.local>
2023-01-05 17:25:19 +0900

    ...

---
+++ 01.RL_101/a_gym_reference_code.py
@@ -0,0 +1,30 @@
+# Create environment
+import gymnasium as gym; print(f"gym.__version__: {gym.__version__}")
+import numpy as np
+np.set_printoptions(edgeitems=10, linewidth=100000, formatter=dict(float=lambda x: "%4.2f" % x))
+
+env = gym.make("LunarLander-v2", render_mode="human")
+
+# Num of training episodes = 3
+for episode in range(3):
+    # Reset
+    observation, info = env.reset(seed=123, options={})
+
+    done = False
+    episode_reward = 0.0
+    while not done:
+        # Agent policy that uses the observation and info
+        action = env.action_space.sample()
+        next_observation, reward, terminated, truncated, info = env.step(action)
+
+        print("Obs.: {0}, Action: {1}, Next Obs.: {2}, Reward: {3:5.2f}, Terminated: {4}, Truncated: {5}, Info: {6}".format(
+            observation, action, next_observation, reward, terminated, truncated, info
+        ))
+
+        episode_reward += reward
+        observation = next_observation
+        done = terminated or truncated
+
+    print(f"Episode: {episode + 1}, Episode Reward: {episode_reward}")
+
+env.close()
---
+++ 01.RL_101/b_gym_env_info.py
@@ -0,0 +1,35 @@
+# https://gymnasium.farama.org/environments/toy_text/frozen_lake/
+import gymnasium as gym; print(f"gym.__version__: {gym.__version__}")
+import numpy as np
+np.set_printoptions(edgeitems=10, linewidth=100000, formatter=dict(float=lambda x: "%7.3f" % x))
+
+env = gym.make("LunarLander-v2", render_mode="human")
+
+def observation_and_action_space_info():
+    #####################
+    # observation space #
+    #####################
+    print("*" * 80)
+    print("[observation_space]")
+    print("env.observation_space: ", env.observation_space)
+    print("env.observation_space.high:", env.observation_space.high)
+    print("env.observation_space.low:", env.observation_space.low)
+    print("env.observation_space.shape:", env.observation_space.shape)
+    for i in range(10):
+        print(env.observation_space.sample())
+    print()
+
+    print("*" * 80)
+    ################
+    # action space #
+    ################
+    print("[action_space]")
+    print(env.action_space)
+    print("env.action_space.n:", env.action_space.n)
+    print("env.action_space.shape:", env.action_space.shape)
+    for i in range(10):
+        print(env.action_space.sample(), end=" ")
+
+
+if __name__ == "__main__":
+    observation_and_action_space_info()
