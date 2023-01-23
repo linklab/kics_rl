265106dddf81faf2d0ebfa47855c72978b7c8b75
Youn-Hee Han <yh21.han@gmail.com>
2023-01-23 17:00:18 +0900

    ...

--- 06.TASK_ALLOCATION/e_dqn_train.py
+++ 06.TASK_ALLOCATION/e_dqn_train.py
@@ -271,13 +271,13 @@ def main():
         "learning_rate": 0.0001,  # 학습율
         "gamma": 0.99,  # 감가율
         "target_sync_step_interval": 500,  # 기존 Q 모델을 타깃 Q 모델로 동기화시키는 step 간격
-        "replay_buffer_size": 30_000,  # 리플레이 버퍼 사이즈
+        "replay_buffer_size": 300_000,  # 리플레이 버퍼 사이즈
         "epsilon_start": 0.95,  # Epsilon 초기 값
         "epsilon_end": 0.01,  # Epsilon 최종 값
         "epsilon_final_scheduled_percent": 0.75,  # Epsilon 최종 값으로 스케줄되는 마지막 에피소드 비율
         "print_episode_interval": 10,  # Episode 통계 출력에 관한 에피소드 간격
         "train_num_episodes_before_next_validation": 200,  # 검증 사이 마다 각 훈련 episode 간격
-        "validation_num_episodes": 15,  # 검증에 수행하는 에피소드 횟수
+        "validation_num_episodes": 30,  # 검증에 수행하는 에피소드 횟수
         "early_stop_patience": env.NUM_TASKS * 50,  # episode_reward가 개선될 때까지 기다리는 기간
     }
 
